f3b7931e257dfc6da64fee093efaba2b2f2323fc
Jordan McCollam <jordy.mccollam@gmail.com>
2025-09-14 12:39:01 -0700

    Added sass and setup screens

---
+++ src/screens/Main/Main.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+import './_main.scss';
+
+const logger = "Screen/Main:: ";
+
+const Main = (props) => {
+
+
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Main;
