67a267fb034458d5f516fcc8c3a20944d069d152
Jordan McCollam <jordy.mccollam@gmail.com>
2025-09-15 02:34:26 -0700

    Added auto scrolling and a dedicated landing page kinda

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import { Gallery, GhanaBorder, Carousel } from "../../components";
+import { ChevronsDown } from "lucide-react";
 import logo from "../../assets/logo-banner.png"
 
 import './_main.scss';
@@ -9,6 +10,19 @@ const logger = "Screen/Main:: ";
 
 const Main = (props) => {
 
+    const scrollToNextSection = (e) => {
+        const section = document.getElementById("gallery-section");
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
+    const scrollToTop = (e) => {
+        const section = document.getElementById("main-section");
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    }
 
     return (
         <Container className="main">
@@ -20,18 +34,18 @@ const Main = (props) => {
                             alt="logo"
                             src={logo}
                             className="logo"
+                            onClick={scrollToTop}
                         />
                         <GhanaBorder full />
                     </Row>
-                    
                 </div>
             </div>
 
-            <Row className="main-content">
-                <Col lg={6} className="d-flex justify-content-center" >
+            <Row className="main-content" id="main-section">
+                <Col lg={5} className="d-flex justify-content-center align-items-center" >
                     <div className="profile-pic"></div>
                 </Col>
-                <Col lg={6} className="d-flex justify-content-center align-items-center" >
+                <Col lg={7} className="d-flex justify-content-center align-items-center" >
                     <div className="bio text-uppercase">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                         <br /><br />
@@ -42,14 +56,16 @@ const Main = (props) => {
                         </div>
                     </div>
                 </Col>
+                <Col xs={12} className="text-center" >
+                    <Button variant="light" className="" onClick={scrollToNextSection}>
+                        <ChevronsDown size={'1.5em'} />
+                    </Button>
+                </Col>
             </Row>
 
+            <div id="gallery-section" />
             <div className="spacer-sm" />
-
             <Carousel />
-
-            <div className="spacer" />
-
             <Gallery />
 
 
