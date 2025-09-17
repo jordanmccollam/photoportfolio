eda5017846d0fb268fe4e47cd885beaf67b918de
Jordan McCollam <jordy.mccollam@gmail.com>
2025-09-16 19:45:32 -0700

    Added full screen capabilities

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -6,6 +6,17 @@ const logger = "Comp/Gallery:: ";
 
 const Gallery = (props) => {
     const totalImages = props.size ? props.size : 20; // number of images in gallery. 8 by default.
+    const [selected, setSelected] = useState(null);
+
+    const onClickPhoto = (photo) => {
+        setSelected(photo.url)
+        document.body.style.overflow = "hidden"; // <- locks background scrolling
+    }
+
+    const onExitFullScreen = () => {
+        setSelected(null)
+        document.body.style.overflow = "auto"; // <- locks background scrolling
+    }
 
     return (
         <div className="gallery" id={props.id ? props.id : 'gallery'}>
@@ -15,8 +26,20 @@ const Gallery = (props) => {
                     src={photo.url}
                     alt={photo.name}
                     className="gallery-item"
+                    onClick={() => onClickPhoto(photo)}
                 />
             ))}
+            
+            {selected && (
+                <div className="gallery-fullscreen" onClick={onExitFullScreen}>
+                    <img
+                        key={`fullscreen-photo`}
+                        src={selected}
+                        alt={'fullscreen-photo'}
+                        className="fullscreen-item"
+                    />
+                </div>
+            )}
         </div>
     )
 }
